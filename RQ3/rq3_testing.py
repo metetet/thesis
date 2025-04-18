6cdf9a70b8f5743ad45b122187dc6c93345f9002
Mete Tugay <115257080+metetet@users.noreply.github.com>
2025-04-18 22:21:05 +0200

    d

---
+++ RQ3/rq3_testing.py
@@ -0,0 +1,66 @@
+# The code below evaluates the fine-tuned model on the faces and art datasets
+
+from transformers import pipeline
+from PIL import Image
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, roc_auc_score
+import os
+import torch
+
+# CUDA check 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load model
+classifier = pipeline("image-classification", model="./sdxl-fine-tune_few_shot", device=0 if torch.cuda.is_available() else -1)
+
+# Load data
+art_dataset_path = 'archive/datasets/art_512x512'
+faces_dataset_path = 'archive/datasets/faces_512x512'
+
+def get_image_paths(dataset_path):
+    image_paths = []
+    labels = []
+    for label in ['0', '1']:
+        folder_path = os.path.join(dataset_path, 'test', label)
+        for filename in os.listdir(folder_path):
+            image_paths.append(os.path.join(folder_path, filename))
+            labels.append(int(label))  # 0 for real, 1 for AI
+    return image_paths, labels
+
+# Evaluate the model on the dataset
+def evaluate_model(image_paths, true_labels, batch_size=32):
+    preds = []
+    
+    for i in range(0, len(image_paths), batch_size):
+        batch_images = [Image.open(path) for path in image_paths[i:i + batch_size]]
+        predictions = classifier(batch_images)
+    
+        for pred in predictions:
+            #print(pred)
+            pred_class = 1 if pred[0]['label'] == '1' else 0 # labels are 0 and 1 for real and AI respectively (load_dataset() uses folder names)
+            preds.append(pred_class)
+    
+    accuracy = accuracy_score(true_labels, preds)
+    precision = precision_score(true_labels, preds)
+    recall = recall_score(true_labels, preds)
+    f1 = f1_score(true_labels, preds)
+    auc = roc_auc_score(true_labels, preds)
+    
+    return accuracy, precision, recall, f1, auc
+
+faces_paths, faces_labels = get_image_paths(faces_dataset_path)
+art_paths, art_labels = get_image_paths(art_dataset_path)
+
+faces_metrics = evaluate_model(faces_paths, faces_labels)
+print(f"Faces Dataset Metrics (Accuracy, Precision, Recall, F1, AUC): {faces_metrics}")
+
+art_metrics = evaluate_model(art_paths, art_labels)
+print(f"Art Dataset Metrics (Accuracy, Precision, Recall, F1, AUC): {art_metrics}")
+
+# Fine-Tuned on Human Dataset + Few-Shot [50] on Art Dataset (sdxl-fine-tune_few_shot)
+# Faces Dataset Metrics (Accuracy, Precision, Recall, F1, AUC): (0.985, 0.9886649874055415, 0.98125, 0.9849435382685069, 0.985)
+# Art Dataset Metrics (Accuracy, Precision, Recall, F1, AUC): (0.2225, 0.03164556962025317, 0.01875, 0.02354788069073783, 0.22250000000000003)
+
+# Fine-Tuned on Human Dataset + Few-Shot [6400] on Art Dataset (sdxl-fine-tune_few_shot)
+# Faces Dataset Metrics (Accuracy, Precision, Recall, F1, AUC): (0.490625, 0.4952681388012618, 0.98125, 0.6582809224318658, 0.490625)
+# Art Dataset Metrics (Accuracy, Precision, Recall, F1, AUC): (0.901875, 0.8552486187845304, 0.9675, 0.9079178885630498, 0.9018749999999999)
+# This one is just to see if the code works correctly (theoretically, should give similar results as mixed-FT)
